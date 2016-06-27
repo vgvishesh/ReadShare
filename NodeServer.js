86ccaa6b76b01eee3908c94beb394644137037ce
vgvishesh <vgvishesh2008@gmail.com>
2016-06-27 07:55:51 +0530

    defining angular routes to put the search query on server

--- NodeServer.js
+++ NodeServer.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.get('/backgroundimage', function (req, res) {
 	var imagePath = "http://127.0.0.1:8080/Images/read4.jpg";
@@ -11,6 +15,11 @@ app.get('/backgroundimage', function (req, res) {
 
 app.get('/andrea.html', function (req, res) {
 	res.sendFile("Andrea.html");
+})
+
+app.post('/search', function (req, res) {
+	console.log(req.body);
+	console.log('search query from the client is ' + req.body.text);
 })
 
 var server = app.listen(8080, function() {
--- public/UiData.js
+++ public/UiData.js
@@ -1,21 +1,24 @@
 var app = angular.module('AppSearchBook', []);
-app.controller('searchController',['$scope','talkToServer', function ($scope, talkToServer) {
+app.controller('searchController',['$scope','getFromServer', 'postToServer', function ($scope, getFromServer, postToServer) {
 	
-	talkToServer("http://127.0.0.1:8080/backgroundimage").then(function(someData) {
+	getFromServer("http://127.0.0.1:8080/backgroundimage").then(function(someData) {
 		$scope.backgroundPhoto = someData.image;
 		console.log($scope.backgroundPhoto);
 	});
 
 	$scope.quote = "You live only once, so make every moment a celebration";
 
-	$scope.bookToSearch = "Look the Book!!";
+	$scope.bookToSearch = {
+		text: "Look the Book!!",
+	};
 
 	$scope.StartSearch = function(value)  {
-		console.log(value);
+		//console.log($scope.bookToSearch);
+		postToServer('http://127.0.0.1:8080/search', $scope.bookToSearch);
 	};
 }]);
 
-app.factory('talkToServer',['$http', function($http) {
+app.factory('getFromServer',['$http', function($http) {
 	return function(serverAddress) 
 	{
 		return $http.get(serverAddress).then(function Success(response) 
@@ -28,5 +31,19 @@ app.factory('talkToServer',['$http', function($http) {
 			console.log(err);			
 			return err;
 		});
+	}
+}]);
+
+app.factory('postToServer', ['$http', function($http) {
+	return function (serverAddress, data)
+	{
+		console.log(data);		
+		return $http.post(serverAddress, data).then(function Success(response) 
+		{
+		},
+		function Error(err)
+		{
+			return err;
+		});		
 	}
 }]);
