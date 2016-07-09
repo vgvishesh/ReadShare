b1ccbc5e7e2adcaa5bcefc902ceefd655919207e
vgvishesh <vgvishesh2008@gmail.com>
2016-07-10 01:39:59 +0530

    adding multiple views, and sharing data between multiple controllers

--- public/AndreaController.js
+++ public/AndreaController.js
@@ -1,7 +1,6 @@
 var app = angular.module('AppSearchBook', ['ngRoute']);
-app.controller('searchController',['$scope','getFromServer', 'postToServer', function ($scope, getFromServer, postToServer) {
+app.controller('searchController',['$scope', '$location', 'getFromServer', 'postToServer', 'saveData', function ($scope, $location, getFromServer, postToServer, saveData) {
 	
-	console.log("vishesh");
 	getFromServer("http://127.0.0.1:8080/backgroundimage").then(function(someData) {
 		$scope.backgroundPhoto = someData.image;
 		console.log($scope.backgroundPhoto);
@@ -9,13 +8,18 @@ app.controller('searchController',['$scope','getFromServer', 'postToServer', fun
 
 	$scope.quote = "You live only once, so make every moment a celebration";
 
+	var initialDisplayText = getInitialDisplayText(saveData);
+	console.log("initialDisplayText: " + initialDisplayText);
+
 	$scope.bookToSearch = {
-		text: "Look the Book!!",
+		text: initialDisplayText,
 	};
 
 	$scope.StartSearch = function()  {
-		//console.log($scope.bookToSearch);
 		postToServer('http://127.0.0.1:8080/search', $scope.bookToSearch);
+		console.log("bookName : " + $scope.bookToSearch.text);
+		saveData.set($scope.bookToSearch.text);
+		loadSearchResultsPage($location);
 	};
 
 	$scope.MonitorKeyStroke = function(keystroke) 
@@ -27,12 +31,34 @@ app.controller('searchController',['$scope','getFromServer', 'postToServer', fun
 	}
 }]);
 
+var loadSearchResultsPage = function(location) {
+	location.path("/results");
+}
+
+
+var getInitialDisplayText = function(saveDataService)
+{
+	var defaultText = "Look the Book!!";
+	var dataSaved = saveDataService.get();
+	console.log("initialSaveData: " + dataSaved);
+	
+	if(!isEmpty(dataSaved)) {
+		return dataSaved;
+	}
+
+	return defaultText;
+}
+
+function isEmpty(obj) {
+    return (Object.getOwnPropertyNames(obj).length === 0);
+}
+
 app.factory('getFromServer',['$http', function($http) {
 	return function(serverAddress) 
 	{
 		return $http.get(serverAddress).then(function Success(response) 
 		{
-			console.log(response.data);
+			console.log("getFromServer " + response.data);
 			return response.data;
 		}, 
 		function Error(err) 
@@ -46,9 +72,8 @@ app.factory('getFromServer',['$http', function($http) {
 app.factory('postToServer', ['$http', function($http) {
 	return function (serverAddress, data)
 	{
-		console.log(data);		
-		return $http.post(serverAddress, data).then(function Success(response) 
-		{
+		console.log("postToServer: " + data);		
+		return $http.post(serverAddress, data).then(function Success(response) {
 		},
 		function Error(err)
 		{
@@ -57,10 +82,29 @@ app.factory('postToServer', ['$http', function($http) {
 	}
 }]);
 
+
+app.factory('saveData', function() {
+	var savedData = {};
+	function set(data) {
+		savedData = data;
+	}
+	function get() {
+		return savedData;
+	}
+	return {
+		set: set,
+		get: get
+	}
+});
+
 app.config(['$routeProvider',function ($routeProvider) {
 	$routeProvider
 	.when('/', {
 		controller:'searchController',
 		templateUrl:'Views/FirstSearchPage.html'
+	})
+	.when('/results', {
+		controller:'searchController',
+		templateUrl:'Views/SearchResult.html'
 	});
 }]);
